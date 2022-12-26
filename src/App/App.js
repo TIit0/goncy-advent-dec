fc87ecaff27e01379ece4b7c2267b206c858901f
TIit0 <viverlin7@gmail.com>
2022-12-26 14:23:46 -0600

    checking edit

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,84 @@
 
 import './App.css';
+import { useReducer, useEffect } from "react"
+import Form from '../components/Form/Form';
+import { ACTIONS, reducer } from '../utils/functions/reducer';
+import Editsvg from '../utils/Svgs/Editsvg/Editsvg';
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    isModalOpen: false,
+    giftList: [{ id: 1672082234700.3264, regalo: 'un regalo', para: 'tish', url: 'https://picsum.photos/seed/picsum/200/300', cantidad: '2' }],
+    editMode: {
+      isEditOn: false,
+      giftToEdit: null,
+    }
+  });
+
+
+
   return (
     <div className="App">
-      
+
+      {
+        state.isModalOpen ?
+
+          <Form
+            dispatch={dispatch}
+            editMode={state.editMode} />
+
+          : null
+      }
+
+      <main className='gift-list'>
+        <h1>Regalos:</h1>
+
+        <button
+          type="button"
+          onClick={() => dispatch({ type: ACTIONS.OPEN_MODAL })}>Agregar Regalo
+        </button>
+
+        <ul>
+          {state.giftList.map(gift => (
+            <li key={gift.id}>
+              <img src={gift.url}></img>
+
+              <div className='li-text'>
+                <p>
+                  {gift.cantidad < 2 ? gift.regalo : `${gift.regalo} x${gift.cantidad}`}
+                </p>
+                <p>{gift.para}</p>
+              </div>
+              <div className='li-button-wrapper'>
+
+                <button
+                  className='li-btn'
+                  onClick={
+                    () => dispatch({
+                      type: ACTIONS.EDIT_ON,
+                      payload: { gift },
+                    })}
+                > <Editsvg />
+                </button>
+
+
+                <button
+                  className='li-btn'
+                  onClick={() => dispatch({
+                    type: ACTIONS.REMOVE_GIFT,
+                    payload: {
+                      id: gift.id
+                    }
+                  })}
+                >X
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+      </main>
     </div>
   );
 }
---
+++ src/utils/functions/reducer.js
@@ -0,0 +1,80 @@
+export const ACTIONS = {
+    OPEN_MODAL: "open",
+    CLOSE_MODAL: "close",
+    ADD_GIFT: "add",
+    REMOVE_GIFT: "remove",
+    EDIT_GIFT: "edit",
+    EDIT_ON: "on",
+    EDIT_OFF: "off",
+}
+
+export function reducer(state, action) {
+    switch (action.type) {
+
+        case ACTIONS.OPEN_MODAL:
+            return { ...state, isModalOpen: true };
+        case ACTIONS.CLOSE_MODAL:
+            return { ...state, isModalOpen: false };
+        case ACTIONS.ADD_GIFT:
+            return {
+                ...state, giftList: [...state.giftList,
+                addGift(
+                    action.payload.gift.regalo,
+                    action.payload.gift.para,
+                    action.payload.gift.url,
+                    action.payload.gift.cantidad
+                )
+                ]
+            };
+        case ACTIONS.REMOVE_GIFT:
+            return {
+                ...state, giftList:
+                    state.giftList.filter(
+                        gift => {
+                            if (gift.id !== action.payload.id) return gift;
+
+                            return null;
+                        })
+            };
+        case ACTIONS.EDIT_GIFT:
+            console.log(action.payload.giftToEdit)
+            return {
+                ...state, giftList:
+                    state.giftList.filter(gift => {
+                        if (gift.id !== action.payload.giftToEdit.id) return gift;
+
+                        gift.regalo = action.payload.giftToEdit.regalo;
+                        gift.para = action.payload.giftToEdit.para;
+                        gift.url = action.payload.giftToEdit.url;
+                        gift.cantidad = action.payload.giftToEdit.cantidad;
+                    })
+            };
+        case ACTIONS.EDIT_ON:
+            return {
+                ...state, isModalOpen: true,
+                editMode: {
+                    isEditOn: true,
+                    giftToEdit: action.payload.gift,
+                }
+            };
+        case ACTIONS.EDIT_OFF:
+            return {
+                ...state, isModalOpen: false,
+                editMode: {
+                    isEditOn: false,
+                    giftToEdit: null,
+                }
+            };
+    }
+}
+
+
+function addGift(regalo, para, url, cantidad) {
+    return {
+        id: Date.now() + Math.random(),
+        regalo,
+        para,
+        url,
+        cantidad
+    }
+}
